452b56e5f7019a7b5ac3ed552b237b4d5666e646
ezio84 <brabus84@gmail.com>
2020-03-01 10:23:20 +0100

    Add api to get or kill foreground app
    We can kang what sysui assistant code does to check if launcher is running,
    using a TaskStackChangeListener to always know the running
    taskid and its component name. This is a solid and reliable way
    to easily kill the task and even remove it from recents without the
    risk to kill launcher or a different app than the foreground one.
    
    Change-Id: I03279ea6139ce5a371e5266e4b35e30f11a3372f
    
    Refactor killapp implementation into a Dependency controller class
    
    There is a lot of good reusable code here that will be used for
    future features. This makes the code universally accessable.
    
    Change-Id: Ic0849ae363a83c9e504771eb3f025b19decfebcd
    
    Taskhelper api: fix NPE
    
    reported while installing an additional launcher from playstore
    
    08-05 14:20:43.787 10550 10593 E AndroidRuntime: FATAL EXCEPTION: android.bg
    08-05 14:20:43.787 10550 10593 E AndroidRuntime: Process: com.android.systemui, PID: 10550
    08-05 14:20:43.787 10550 10593 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'java.lang.String android.content.ComponentName.getPackageName()' on a null object reference
    08-05 14:20:43.787 10550 10593 E AndroidRuntime: 	at com.android.systemui.statusbar.policy.TaskHelper.isLauncherShowing(TaskHelper.java:277)
    08-05 14:20:43.787 10550 10593 E AndroidRuntime: 	at com.android.systemui.statusbar.policy.TaskHelper$3.run(TaskHelper.java:145)
    
    TaskHelper: Update callbacks on overlay change
    
    Much of the statusbar is recreated on a overlay change so callbacks may need a refresh. Fixes clock disappearing after theme change.
    
    Change-Id: Iae34bef2ebabb708e1d7b2d192f5e940ace80864
    
    TaskHelper: allow to get foreground app label
    Change-Id: Ic3d9d3a7e379c45573d6378fce1e6399acbb75bd
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -167,6 +167,7 @@ public class CommandQueue extends IStatusBar.Stub implements
     private static final int MSG_SCREEN_PINNING_STATE_CHANGED = 102 << MSG_SHIFT;
     private static final int MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED = 103 << MSG_SHIFT;
     private static final int MSG_TOGGLE_SETTINGS_PANEL = 104 << MSG_SHIFT;
+    private static final int MSG_KILL_FOREGROUND_APP = 105 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -482,6 +483,8 @@ public class CommandQueue extends IStatusBar.Stub implements
 
         default void screenPinningStateChanged(boolean enabled) {}
         default void leftInLandscapeChanged(boolean isLeft) {}
+
+        default void killForegroundApp() { }
     }
 
     public CommandQueue(Context context) {
@@ -1307,6 +1310,13 @@ public class CommandQueue extends IStatusBar.Stub implements
         }
     }
 
+    public void killForegroundApp() {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_KILL_FOREGROUND_APP);
+            mHandler.sendEmptyMessage(MSG_KILL_FOREGROUND_APP);
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -1762,6 +1772,11 @@ public class CommandQueue extends IStatusBar.Stub implements
                 case MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).leftInLandscapeChanged(msg.arg1 != 0);
+                    }
+                    break;
+                case MSG_KILL_FOREGROUND_APP:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).killForegroundApp();
                     }
                     break;
             }
