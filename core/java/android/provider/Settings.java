ed25b107f3a054a224ee90d84a1f7185b51fb9a5
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-05-14 00:53:09 +0200

    AutoBrightness: Add support for one shot auto-brightness
    - Only update auto brightness one time when screen is turned on.
    - Can be useful on devices where sensor is not accurate.
    
    Author: Eamon Powell <eamonpowell@outlook.com>
    Date:   Sun Aug 29 11:37:44 2021 +1000
    
        fixup! AutoBrightness: Add support for one shot auto-brightness:
    
        * Remove unnecessary newlines
        * Fix incorrect indentation
        * Fix typo in comments
    
        Change-Id: I7d6071f4344605b3dcc3f1c33ac4390f1020e03f
    
    Change-Id: I5187fdb7765550d4eaa999317ab95f487d44c8d8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6148,6 +6148,13 @@ public final class Settings {
         public static final String FORCE_FULLSCREEN_CUTOUT_APPS = "force_full_screen_cutout_apps";
 
         /**
+         * Whether auto brightness is applied one shot when screen is turned on.
+         * Default 0
+         * @hide
+         */
+        public static final String AUTO_BRIGHTNESS_ONE_SHOT = "auto_brightness_one_shot";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
