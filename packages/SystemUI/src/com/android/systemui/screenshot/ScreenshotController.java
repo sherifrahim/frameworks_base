2b4978c5efe18bc2041f35501e82879d67ad6841
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-26 01:39:00 +0300

    SystemUI: screenshot: disable magnification for partial screenshots
    Partial screenshots are screen-size and don't need a magnifying glass.
    
    Change-Id: I472103352411d1482423230a4ff08ddda45cc53d

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -762,6 +762,7 @@ public class ScreenshotController {
         ScrollCaptureController.BitmapScreenshot bitmapScreenshot =
             new ScrollCaptureController.BitmapScreenshot(mContext, newScreenshot);
 
+        mLongScreenshotHolder.setNeedsMagnification(false);
         startLongScreenshotActivity(bitmapScreenshot);
     }
 
@@ -830,6 +831,7 @@ public class ScreenshotController {
                 return;
             }
 
+            mLongScreenshotHolder.setNeedsMagnification(true);
             startLongScreenshotActivity(longScreenshot);
         }, mMainExecutor);
     }
